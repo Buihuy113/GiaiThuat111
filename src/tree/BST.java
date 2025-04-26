9db9cde70d643c972047e6df36e60abbcc1a701e
Buihuy113 <162303539+Buihuy113@users.noreply.github.com>
2025-04-26 22:45:47 +0700

    commit BST

---
+++ src/tree/BST.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tree;
+
+/**
+ *
+ * @author duchuy
+ */
+public class BST {
+    public static void main(String[] args){
+        System.out.println("hello word!!!");
+    }
+}
